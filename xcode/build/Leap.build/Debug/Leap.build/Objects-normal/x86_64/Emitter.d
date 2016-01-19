fe09dd63dd30dce4acc181947d3485c4a802b287
ichitomo <ichitomo0421@gmail.com>
2016-01-19 13:44:57 +0900

    指の代わりの追加

---
+++ xcode/build/Leap.build/Debug/Leap.build/Objects-normal/x86_64/Emitter.d
@@ -0,0 +1,4 @@
+dependencies: /Users/ichinosetomohiro/Desktop/Leap/src/Emitter.cpp \
+  /Users/ichinosetomohiro/Desktop/Leap/include/Emitter.h \
+  ../include/Particle.h \
+  ../../cinder_0.8.6_mac/include/cinder/gl/Texture.h
